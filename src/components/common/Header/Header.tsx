df26950c6a7da79add97688e7ac07c88d7c7313b
Schalk Burger <schalkb@gmail.com>
2020-06-12 04:48:41 +0200

    Add Video background test

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -17,13 +17,13 @@ export const Header: React.FC<HeaderProps> = ({ ...props }) => {
   return (
     <div className="header-section">
       <div className="container header">
-        <div className="sticky">
+        <div className="position-fixed flex flex-column nav-container min-vw-15">
           <h1 className="logo">
             <Link to="/">
               <CompassGlassLogoSVG />
             </Link>
           </h1>
-          <nav className="cl-effect-21">
+          <nav>
             <Link to="/about">About</Link>
             <Link to="/faq">FAQ</Link>
             <Link to="/technical">Technical</Link>
--- src/components/layouts/BasicLayout/BasicLayout.tsx
+++ src/components/layouts/BasicLayout/BasicLayout.tsx
@@ -3,12 +3,21 @@ import { Helmet } from 'react-helmet-async';
 import { LayoutProps } from 'application/models';
 import { Header, Footer } from 'components/common';
 
+// let backgroundVideoPoster = require('../../../assets/images/Compass-Glass-Background.jpg');
+// let backgroundVideo = require('../../../assets/videos/Compass-Glass-Video.mp4');
+
 /**
  * Basic page layout component
  */
 export const BasicLayout: React.FC<LayoutProps> = ({ children, headMeta }) => {
   return (
     <div className="basic-layout">
+      {/* <div className="video-wrapper">
+        <video playsInline autoPlay muted loop poster={backgroundVideoPoster}>
+          {/* <source src="polina.webm" type="video/webm" /> */}
+      {/* <source src={backgroundVideo} type="video/mp4" /> */}
+      {/* </video> */}
+      {/* </div> */}
       <Helmet>
         <title>{headMeta.title}</title>
         {headMeta.meta.map((metaItem) => (
