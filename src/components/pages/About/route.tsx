9b01f1d883256156be4cb1069cb5c7ec05913a5b
Schalk Burger <schalkb@gmail.com>
2020-07-07 01:59:37 +0200

    Meta update for About

--- src/components/pages/About/route.tsx
+++ src/components/pages/About/route.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { RouteConfig } from 'application/models';
 
 export const aboutRoute: RouteConfig = {
-	name: 'about',
-	layout: {
+  name: 'about',
+  layout: {
     component: React.lazy(() =>
       import('components/layouts/BasicLayout/default')
     ),
@@ -12,13 +12,13 @@ export const aboutRoute: RouteConfig = {
     path: '/about',
     component: React.lazy(() => import('components/pages/About/default')),
   },
-	headMeta: {
-    title: 'About page',
-		meta: [
-			{
-				name: 'description',
-				content: 'About compass glass'
-			},
-		],
+  headMeta: {
+    title: 'About Compass Glass',
+    meta: [
+      {
+        name: 'description',
+        content: 'About Compass Glass',
+      },
+    ],
   },
 };
