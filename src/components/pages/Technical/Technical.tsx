1a50f798c59966774e67c0df507246efcacf3ec4
Schalk Burger <schalkb@gmail.com>
2020-07-26 11:16:58 +0200

    Update FAQ and Technical pages

--- src/components/pages/Technical/Technical.tsx
+++ src/components/pages/Technical/Technical.tsx
@@ -45,40 +45,56 @@ export const Technical: React.FC<TechnicalProps> = ({ ...props }) => {
               </thead>
               <tbody>
                 <tr>
-                  <td rowSpan={4}>Mined or Quarried Materials</td>
+                  <td rowSpan={4} className="cell-origin">
+                    Mined or Quarried Materials
+                  </td>
                   <td>Silica</td>
                   <td>Si</td>
                   <td>72</td>
                 </tr>
                 <tr>
                   <td>Limestone</td>
-                  <td>CaCO 3</td>
+                  <td>CaCO³</td>
                   <td>9</td>
                 </tr>
                 <tr>
                   <td>Dolomite</td>
-                  <td>CaMg(CO 3 ) 2</td>
+                  <td>CaMg(CO³)²</td>
                   <td>4</td>
                 </tr>
                 <tr>
                   <td>Others</td>
-                  <td>incl iron oxide **</td>
+                  <td>incl iron oxide *</td>
                   <td>1</td>
                 </tr>
                 <tr>
-                  <td>Manufactured Materials</td>
+                  <td className="cell-origin">Manufactured Materials</td>
                   <td>Soda Ash</td>
-                  <td>Na 2 CO 3</td>
+                  <td>Na²CO³</td>
                   <td>14</td>
                 </tr>
                 <tr>
-                  <td>By-Product Material</td>
+                  <td className="cell-origin">By-Product Material</td>
                   <td>Clean Scrap</td>
                   <td>“Cullet”</td>
                   <td>Up to 20% by volume</td>
                 </tr>
               </tbody>
             </table>
+            <small>
+              <sup>*</sup> Note: All commercial window glass contains a residue
+              of iron oxide (Fe2O3 ) which is difficult and expensive to remove
+              and gives the glass it’s characteristic green edge-colour which is
+              considered acceptable for most glazing applications, including
+              mirrors. However, to meet the demand for a “whiter” architectural
+              window glass, it is possible to remove most of the residual iron
+              by further processing and additional cost to create Low-Iron
+              (“Extra-Clear”) glass which is almost perfectly white with no
+              green colour at the edges. The clarity and high light-transmission
+              characteristics of “Low-Iron” glass make it a highly popular
+              glazing-choice for a wide variety of applications in Retail and
+              Commercial buildings.
+            </small>
           </div>
         </div>
       </div>
