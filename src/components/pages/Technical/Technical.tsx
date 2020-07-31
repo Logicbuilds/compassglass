8e289723ca7c6a550ba7b512231ad1104781a05e
Stephanus Venter <stephanus@getshogun.com@users.noreply.github.com>
2020-07-31 17:41:02 +0200

    Configured technical page

--- src/components/pages/Technical/Technical.tsx
+++ src/components/pages/Technical/Technical.tsx
@@ -1,39 +1,23 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
-export interface TechnicalProps {
-  /** Example Technical property. */
-  exampleTechnical?: string;
-}
+import techData from 'assets/content/pages/technical.json';
 
 /**
  * Technical inforamatioin about related topics
  */
-export const Technical: React.FC<TechnicalProps> = ({ ...props }) => {
+export const Technical: React.FC = () => {
   return (
     <div className="section">
       <div className="page container">
         <div className="cards-container glass frosted-glass">
           <div className="card card-padding-default">
-            <h1>Technical</h1>
-            <h2>What is glass?</h2>
-            <p>
-              Glass is a material with remarkable properties of fragility,
-              durability and transparency and is an essential component of
-              civilised life as we know it in the modern world. It protects us
-              from short-term changes in the weather as well as from longer-term
-              changes in the climate and provides us with a view of the world
-              beyond the pane. If undisturbed, glass will continue to perform
-              it’s protective role for centuries but, if disturbed by impact,
-              excessive wind-load or other undue forces, it will break into
-              fragments which can be potentially lethal. This unique combination
-              of durability and fragility are enhanced by modern processing
-              methods which provide a wide range of benefits of additional
-              strength and security and allow glass to be used in a vast range
-              of applications in all aspects of modern living.
-            </p>
+            <h1>{techData.title}</h1>
+            <h2>{techData.whatIsGlass.title}</h2>
+            <ReactMarkdown source={techData.whatIsGlass.content} />
 
-            <h2>Composition of commercial glass</h2>
-            <h3>Soda lime glass</h3>
+            <h2>{techData.compositionOfCommercialGlass.title}</h2>
+            <h3>{techData.compositionOfCommercialGlass.subTitle}</h3>
 
             <div className="table-container">
               <table className="table striped">
@@ -83,6 +67,7 @@ export const Technical: React.FC<TechnicalProps> = ({ ...props }) => {
                 </tbody>
               </table>
             </div>
+            {/* <ReactMarkdown source={techData.disclaimer} /> */}
             <small>
               <sup>*</sup> Note: All commercial window glass contains a residue
               of iron oxide (Fe2O3 ) which is difficult and expensive to remove
