893033ec87a284e2ca740e8a82bce3c19f57dbdd
Schalk Burger <schalkb@gmail.com>
2026-04-16 02:29:59 +0200

    Update Terms

--- src/components/pages/Terms/Terms.tsx
+++ src/components/pages/Terms/Terms.tsx
@@ -14,7 +14,184 @@ export const Terms: React.FC = () => {
           <div className="card card-padding-default">
             <h1>{termsData.title}</h1>
             {/* @ts-ignore */}
-            <p>Terms and Conditions</p>
+            <h2>Acceptance of Terms</h2>
+
+            <p>
+              The Supplier permits the use of this Website subject to these
+              terms and conditions ("the Terms and Conditions"). By using this
+              Website in any way, you shall be deemed to have accepted all the
+              Terms and Conditions unconditionally. You must not use this
+              Website if you do not agree to the Terms and Conditions. In the
+              event of any conflict between the Terms and Conditions and any
+              other conditions stipulated elsewhere, including in one of our
+              Suppliers, the Terms and Conditions as contained herein will
+              prevail.
+            </p>
+
+            <h2>Use of the Website</h2>
+
+            <p>
+              The contents of this Website, including any content, information,
+              software, icons, text, links, graphics, lay-outs, images, sound
+              clips, trade names, logos, trademarks, and service marks are
+              protected by law, including but not limited to copyright and
+              trademark law, and are owned by or licensed to The Supplier. No
+              license to or right in any of such contents is granted to or
+              conferred upon you. Any unauthorized use, distribution or
+              reproduction of the said contents is prohibited.
+            </p>
+
+            <p>
+              By entering this Website, you agree that you will not use any
+              device, software, or other instrumentality to interfere or attempt
+              to interfere with the proper working of the Website and that you
+              will not take any action that imposes an unreasonable or
+              disproportionately large load on our infrastructure. In addition,
+              you agree that you will not use any robot, spider, other automatic
+              device, or manual process to monitor or copy the pages of the
+              Website or the content contained herein, without the prior written
+              consent from an authorized The Supplier representative (such
+              consent is deemed given for standard search engine technology
+              employed by Internet search websites to direct Internet users to
+              this Website). You may not use the Website to distribute material,
+              which is defamatory, offensive, and unlawful or contains hate
+              speech.
+            </p>
+
+            <h2>Disclaimer</h2>
+
+            <p>
+              While The Supplier takes reasonable measures to ensure that the
+              contents of this Website are accurate and complete, The Supplier
+              makes no representations or warranties, whether express or
+              implied, as to the quality, timeliness, operation, integrity,
+              availability or functionality of this Website or as to the
+              accuracy, completeness or reliability of any information on this
+              Website. The Supplier reserves the right to make changes,
+              corrections, and/or improvements to the information and to the
+              products and programs described in such information, at any time
+              without notice.
+            </p>
+
+            <p>
+              The Supplier will use reasonable endeavours to maintain the
+              availability of the Website, except during scheduled maintenance
+              periods, and reserves the right to discontinue providing the
+              Website or the service or any part thereof with or without notice
+              to you. Any person who accesses this Website or relies on this
+              Website or on the information contained in this Website does so at
+              his or her own risk.
+            </p>
+
+            <p>
+              In addition to the disclaimers contained elsewhere in these Terms
+              and Conditions, The Supplier also makes no warranty or
+              representation, whether express or implied, that the information
+              or files available on this Website are free of viruses, spyware,
+              malware, trojans, destructive materials or any other data or code
+              which is able to corrupt, destroy, compromise or jeopardise the
+              operation, stability, security functionality or content of your
+              computer system, computer network, hardware or software in any
+              way. The Supplier does not accept any responsibility for any
+              errors or omissions on this Website.
+            </p>
+
+            <h2>Linked Third-Party Website and Third-Party Content</h2>
+
+            <p>
+              This Website may contain links or references to other websites
+              outside of our control, including those of advertisers. These
+              Terms and Conditions do not apply to those websites and The
+              Supplier is not responsible for the practices and/or privacy
+              policies of those sites or the cookies those sites use. In
+              addition, because The Supplier has no control over such external
+              sites and resources, you acknowledge and agree that The Supplier
+              is not responsible for ensuring the availability of such external
+              websites or resources and does not endorse and is not responsible
+              or liable for any content, advertising, products, or other
+              materials on or available from such websites or resources. Your
+              use of such other websites is entirely at your own risk and we are
+              not responsible for any loss, expense, claim, or damage, whether
+              direct, indirect or consequential, arising from your use of such
+              other websites or your reliance on any information contained on
+              those websites.
+            </p>
+
+            <h2>
+              Permission for Hyperlinks, Deep Linking, Crawlers and Metatags
+            </h2>
+
+            <p>
+              Nobody may establish a hyperlink, frame, metatag or similar
+              reference, whether electronically or otherwise (collectively
+              referred to as linking), to this Website or any subsidiary pages
+              before receiving the prior written approval of an authorised
+              representative of The Supplier, which may be withheld or granted
+              subject to such conditions The Supplier may specify from time to
+              time. Furthermore, this Website or any part hereof may not be
+              "framed" or "deep linked" in any way whatsoever. This Website may
+              from time to time contain message boards which allow users to
+              comment on their experience at The Supplier. At times, those
+              comments may contain references to matters not related to The
+              Supplier. Those references do not necessarily represent the views
+              of The Supplier.
+            </p>
+
+            <h2>Limitation of Liability</h2>
+
+            <p>
+              THE SUPPLIER SHALL IN NO WAY BE LIABLE FOR ANY DIRECT, INDIRECT,
+              INCIDENTAL, SPECIAL OR CONSEQUENTIAL INJURY, LOSS OR DAMAGES WHICH
+              MIGHT ARISE FROM YOUR USE OF, OR RELIANCE UPON, ANY MATERIAL OR
+              CONTENT CONTAINED IN THE WEBSITE OR ANY LINKED WEBSITE, OR ANY
+              INABILITY TO USE THE WEBSITE, OR ANY UNLAWFUL ACTIVITY ON THE
+              WEBSITE CAUSED BEYOND THE REASONABLE CONTROL OF THE SUPPLIER.
+            </p>
+
+            <p>
+              YOU HEREBY INDEMNIFY THE SUPPLIER AGAINST ANY LOSS, CLAIM OR
+              DAMAGE WHICH MAY BE SUFFERED BY YOURSELF OR ANY THIRD PARTY
+              ARISING IN ANY WAY FROM YOUR USE OF THIS WEBSITE OR THE RECEIPT BY
+              YOU OF AN ELECTRONIC MAIL FROM THE SUPPLIER OR ONE THAT PURPORTS
+              TO EMANATE FROM THE SUPPLIER, UNLESS THE LOSS, CLAIM OR DAMAGE
+              ARISES AS A RESULT OF THE GROSS NEGLIGENCE OF THE SUPPLIER.
+            </p>
+
+            <h2>Changes to these Terms and Conditions</h2>
+
+            <p>
+              The Supplier reserves the right to update and/or amend the Terms
+              and Conditions from time to time and without any notice, and you
+              are accordingly encouraged to check the Website regularly. Any
+              such change will only apply to your use of this Website after the
+              change is displayed on this Website. If you use this Website after
+              such updated or amended Terms and Conditions have been displayed
+              on this Website, you will be deemed to have accepted such updates
+              or amendments.
+            </p>
+
+            <h2>Termination</h2>
+
+            <p>
+              The Supplier may in its sole discretion terminate, suspend, and
+              modify this Website and/or your use of this Website, with or
+              without notice to you. You agree that The Supplier will not be
+              liable to you if it chooses to suspend, modify or terminate this
+              Website or your use of this Website.
+            </p>
+
+            <h2>Governing Law</h2>
+
+            <p>
+              The Terms and Conditions shall be governed and interpreted in
+              accordance with the laws of the Republic of South Africa and
+              application for any of the services offered on these pages or
+              sites will constitute your consent and submission to the
+              jurisdiction of the South African courts regarding all
+              proceedings, transactions, applications or the like instituted by
+              either party against the other, arising from any of the terms and
+              conditions pertaining to such services.
+            </p>
           </div>
         </div>
       </div>
